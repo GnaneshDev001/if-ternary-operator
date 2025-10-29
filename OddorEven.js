356ab72d795a448117a9c43accb06876da11fa24
Gnanesh <gnaneshmahi4@gmail.com>
2025-10-29 11:21:06 +0530

    text

---
+++ OddorEven.js
@@ -0,0 +1,11 @@
+n=22;
+if(n%2==0){
+    console.log("Even");
+}
+else{
+    console.log("Odd");
+}
+
+
+let num=22;
+num%2==0 ? console.log("Even") : console.log("Odd");
---
+++ threedigitORnot.js
@@ -0,0 +1,12 @@
+//ternary operator
+let num=451
+num>=100 && num<=1000 ? console.log("Yes"):
+                        console.log("No")
+//if else
+ if(num>=100 && num<=1000){
+    console.log("Yes!Given Number is 3 Digit No")
+}
+else{
+    console.log("Not a 3 Digit No")
+
+} 
